870ffad05c6a32bdc84ebae36bed9eb247c77de1
Melvin Chia <melvinchia623600@gmail.com>
2025-07-20 08:19:24 +0800

    25w29

--- src/pages/modals/index.ts
+++
@@ -1,7 +0,0 @@
-import { type ModalComponent } from '@lifeforge/ui'
-
-import CreateEntryModal from './CreateEntryModal'
-
-export const LocaleManagerModals: Record<string, ModalComponent> = {
-  'localeManager.createEntry': CreateEntryModal
-}
